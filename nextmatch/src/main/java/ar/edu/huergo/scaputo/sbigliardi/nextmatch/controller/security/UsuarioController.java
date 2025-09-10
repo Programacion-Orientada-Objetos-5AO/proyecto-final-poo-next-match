0bdc1bb455dc1c9d2b8a43a983ee9ea7d531b68e
Santiago9-12-18-4-3-1 <sbigliardi@alumno.huergo.edu.ar>
2025-09-09 22:15:44 -0300

    .

--- nextmatch/src/main/java/ar/edu/huergo/scaputo/sbigliardi/nextmatch/controller/security/UsuarioController.java
+++ nextmatch/src/main/java/ar/edu/huergo/scaputo/sbigliardi/nextmatch/controller/security/UsuarioController.java
@@ -1,5 +1,42 @@
 package ar.edu.huergo.scaputo.sbigliardi.nextmatch.controller.security;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.edu.huergo.scaputo.sbigliardi.nextmatch.dto.security.RegistrarDTO;
+import ar.edu.huergo.scaputo.sbigliardi.nextmatch.dto.security.UsuarioDTO;
+import ar.edu.huergo.scaputo.sbigliardi.nextmatch.entity.security.Usuario;
+import ar.edu.huergo.scaputo.sbigliardi.nextmatch.mapper.security.UsuarioMapper;
+import ar.edu.huergo.scaputo.sbigliardi.nextmatch.service.security.UsuarioService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/equipo/usuarios")
 public class UsuarioController {
-    
+    @Autowired
+    private UsuarioService usuarioService;
+    @Autowired
+    private UsuarioMapper usuarioMapper;
+
+    @PostMapping("/registrar")
+    public ResponseEntity<UsuarioDTO> registrarCliente(@Valid @RequestBody RegistrarDTO registrarDTO) {
+        Usuario usuario = usuarioMapper.toEntity(registrarDTO);
+        Usuario nuevoUsuario = usuarioService.registrar(usuario, registrarDTO.password(), registrarDTO.verificacionPassword());
+        UsuarioDTO nuevoUsuarioDTO = usuarioMapper.toDTO(nuevoUsuario);
+        return ResponseEntity.ok(nuevoUsuarioDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> getAllUsuarios() {
+        List<Usuario> usuarios = usuarioService.getAllUsuarios();
+        List<UsuarioDTO> usuarioDTOs = usuarioMapper.toDTOList(usuarios);
+        return ResponseEntity.ok(usuarioDTOs);
+    }
 }
