9c7404897633fba80a69b47d6fbb22bb26812b9b
Santiago <sbigliardi@alumno.huergo.edu.ar>
2025-11-25 22:03:28 -0300

    repository y service

---
+++ nextmatch/src/main/java/ar/edu/huergo/scaputo/sbigliardi/nextmatch/repository/UsuarioInventarioRepository.java
@@ -0,0 +1,4 @@
+package ar.edu.huergo.scaputo.sbigliardi.nextmatch.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ar.edu.huergo.scaputo.sbigliardi.nextmatch.entity.security.UsuarioInventario;
+public interface UsuarioInventarioRepository extends JpaRepository<UsuarioInventario, Integer> {}
---
+++ nextmatch/src/main/java/ar/edu/huergo/scaputo/sbigliardi/nextmatch/service/UsuarioIneventarioService.java
@@ -0,0 +1,59 @@
+package ar.edu.huergo.scaputo.sbigliardi.nextmatch.service;
+
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import ar.edu.huergo.scaputo.sbigliardi.nextmatch.dto.UsuarioInventarioDTO;
+
+@Service
+public class UsuarioIneventarioService {
+
+    private static final String API_KEY = "c9ae24e836d3d8248578568d6d68cce7";
+    private static final String BASE_URL = "https://v3.football.api-sports.io/";
+
+
+    public List<UsuarioInventarioDTO> obtenerUsuariosPorNombre(String nombreUsuario) throws Exception {
+        String nombreCodificado = URLEncoder.encode(nombreUsuario, StandardCharsets.UTF_8);
+        String url = BASE_URL + "users?name=" + nombreCodificado;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("x-apisports-key", API_KEY)
+                .header("x-apisports-host", "v3.football.api-sports.io")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        JSONObject json = new JSONObject(response.body());
+        JSONArray usuariosArray = json.getJSONArray("response");
+
+        List<UsuarioInventarioDTO> usuarios = new ArrayList<>();
+
+        for (int i = 0; i < usuariosArray.length(); i++) {
+            JSONObject usuarioObj = usuariosArray.getJSONObject(i).getJSONObject("user");
+
+            UsuarioInventarioDTO dto = new UsuarioInventarioDTO();
+            dto.setId(usuarioObj.getLong("id"));
+            dto.setNombre(usuarioObj.getString("name"));
+            dto.setCategoria(usuarioObj.getString("categoria"));
+            dto.setPrecio(usuarioObj.getDouble("precio"));
+            dto.setStock(usuarioObj.getInteger("stock"));
+
+            usuarios.add(dto);
+        }
+
+        return usuarios;
+    }
+}
+
